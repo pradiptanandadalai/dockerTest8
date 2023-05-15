d56aed0387a81ea6c8f4960bac5d2597bce48a85
pd1061 <pradiptananda.dalai@commscope.com>
2023-05-15 13:44:52 +0530

    Update docker file

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk
 COPY target/*.jar /
 EXPOSE 8080
-ENTRYPOINT ["java","-jar","/com-0.0.1-SNAPSHOT.jar"]
+ENTRYPOINT ["java","-jar","/dockerized-jar.jar.jar"]
--- src/main/java/com/test/docker/controller/GetString.java
+++ src/main/java/com/test/docker/controller/GetString.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Test {
+public class GetString {
 	
 	@RequestMapping(value = "/get")
 	public String getResult(){
